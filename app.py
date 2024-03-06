366a5b268195f31b80473e3ef3de6bafbae16608
anastasia chudakova <chuan22git@gmail.com>
2024-03-06 18:45:37 +0300

    adding to .gitignore

--- app.py
+++ app.py
@@ -1,122 +1,102 @@
-from flask import Flask, render_template, abort
+from flask import Flask, request, render_template, abort, jsonify
+from pathlib import Path
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+BASE_DIR = Path(__file__).parent
 
-name = "Владимир"
+app.config["JSON_AS_ASCII"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{BASE_DIR / 'users.db'}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class UserModel(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(32), nullable=False)
+    last_name = db.Column(db.String(32), nullable=False)
+    name = db.Column(db.String(32), nullable=False)
+    surname = db.Column(db.String(32), nullable=False)
+    birth_date = db.Column(db.String(32))
+    phone_number = db.Column(db.String(32), nullable=False)
+
+    def __init__(self, login, last_name, name, surname, birth_date, phone_number):
+        self.login = login
+        self.last_name = last_name
+        self.name = name
+        self.surname = surname
+        self.birth_date = birth_date
+        self.phone_number = phone_number
+
+    def __repr__(self):
+        return f"User({self.login})"
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "login": self.login,
+            "last_name": self.last_name,
+            "name": self.name,
+            "surname": self.surname,
+            "birth_date": self.birth_date,
+            "phone_number": self.phone_number,
+        }
+
 
 @app.get("/")
 def home():
+
     return render_template("index.html")
 
-'''
-@app.route("/names")
-def names():
-    return render_template('names.html', name = name)
-'''
 
 @app.get("/names")
 def names():
+    users = UserModel.query.all()
     entities = list()
-    with open("files/names.txt", encoding="utf-8") as f:
-        for raw_line in f:
-            entities.append(raw_line.strip())
-    return render_template('names.html', entities = entities)
+    for user in users:
+        entities.append(user.name)
+    return render_template("names.html", entities=entities)
 
 
 @app.get("/table")
 def humans():
+    users = UserModel.query.all()
     entities = list()
-    with open("files/humans.txt", encoding="utf-8") as f:
-        for raw_line in f:
-            data = raw_line.strip().split(';')
-            keys = ['last_name', 'name', 'surname']
-            item = dict()
-            for i in range(len(data)):
-                item[keys[i]] = data[i]
-            entities.append(item)
-    return render_template('table.html', entities = entities)
-'''
-Или можно так создать таблицу
-data = raw_line.strip().split(';')
-entities.append({'last_name': data[0], 
-                               'name': data[1], 'surname': data[2]})
-'''
+    for user in users:
+        keys = ["last_name", "name", "surname"]
+        values = [user.last_name, user.name, user.surname]
+        item = dict(zip(keys, values))
+        entities.append(item)
+    return render_template("table.html", entities=entities)
 
-@app.route('/users')
+
+@app.route("/users")
 def users_list():
+    users = UserModel.query.all()
     entities = list()
-    with open("files/users.txt", encoding="utf-8") as f:
-        for raw_line in f:
-            data = raw_line.strip().split(';')
-            keys = ['username', 'last_name', 'name', 
-                    'surname', 'birth_date', 'phone_number']
-            item = dict()
-            for i in range(len(data)):
-                item[keys[i]] = data[i]
-            entities.append(item)
-    return render_template('users_list.html', entities = entities)
-
-'''и можно еще сделать вот так **{'entities': entities}'''
-
-'''Можно еще создать вот так            
-entities.append(dict(zip(('login', 'last_name', 'name',
-                     'surname', 'birth_date', 'phone'), data))'''
+    for user in users:
+        entities.append(user)
+    return render_template("users_list.html", entities=entities)
 
 
 @app.route("/users/<login>")
-def user_item(login):
-    item = None
-    with open('files/users.txt', encoding="utf-8") as f:
-        for raw_line in f:
-            data = raw_line.strip().split(';')
-            if data[0] == login:
-                item = {
-                    'login': data[0],
-                    'last_name': data[1],
-                    'name': data[2],
-                    'surname': data[3],
-                    'birth_date': data[4],
-                    'phone': data[5]
-                }
-                break
-    if item is None:
-        abort(404, f'User with login: {login} not found')
-    return render_template('user_info.html', item=item)
-
-'''
-@app.route('/users/<login>')
-def user_item(login):
-    entities = list()
-    user = list()
-    with open("files/users.txt", encoding="utf-8") as f:
-        for raw_line in f:
-            data = raw_line.strip().split(';')
-            keys = ['username', 'last_name', 'name', 
-                    'surname', 'birth_date', 'phone_number']
-            item = dict()
-            for i in range(len(data)):
-                item[keys[i]] = data[i]
-            entities.append(item)
-    for e in entities:
-        if e['username'] == login:
-            user.append(e)
-    if len(user) == 0:
-        abort(404, f'User with login: {login} not found')    
-    return render_template('user_item.html', user = user)
-'''
-
-
-'''
-@app.route('/posts/<int:post_id>')
-def show_post(post_id):
-    # показывает статью по её id (int)
-    return f'Post {post_id}'
-'''
+def get_user_by_login(login):
+    users = UserModel.query.all()
+    for user in users:
+        if user.login == login:
+            return render_template("user_info.html", item=user)
+    else:
+        abort(404, f"User with login = {login} not found")
 
 
 @app.route("/about")
 def about():
     return "О нас"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
